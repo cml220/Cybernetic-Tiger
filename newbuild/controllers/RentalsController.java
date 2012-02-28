7f4fd06bc04dd88a7e3d7cbfed7be57255a63716
Jason Knight <00jknight@gmail.com>
2012-02-28 10:49:46 -0600

    Updated rentals, catalogue, login controller

--- newbuild/controllers/RentalsController.java
+++ newbuild/controllers/RentalsController.java
@@ -11,21 +11,20 @@ public class RentalsController {
 	
 	public void addBookToCurrentUser(Book book)
 	{
-		
+		Controller.getCurrentUser().rentals.add(book);
 	}
 	public void addBookToUser(Book book, User user)
 	{
-		
+		user.rentals.add(book);
 	}
 	
 	public void removeBookFromUser(Book book, User user)
 	{
-		
+		user.rentals.remove(book);
 	}
 	
 	public void removeBookFromCurrentUser(Book book)
 	{
-		
-		
+		Controller.getCurrentUser().rentals.remove(book);		
 	}
 }
