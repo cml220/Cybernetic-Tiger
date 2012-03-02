11bfbc1632f93d8500dcb2c206e7a464b202b850
baj231 <baj231@mail.usask.ca>
2012-03-01 21:35:56 -0600

    Corrected some layout issues in GUI
    Signed-off-by: baj231 <baj231@mail.usask.ca>

--- newbuild/gui/CheckoutPaymentFieldsPanel.java
+++ newbuild/gui/CheckoutPaymentFieldsPanel.java
@@ -6,43 +6,41 @@ package gui;
  * 
  */
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.FlowLayout;
 
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CheckoutPaymentFieldsPanel extends JPanel {
 
-	
-	
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4885426338224971236L;
-	
-	private JLabel text;
-	private JTextField field;		
-	
-	public CheckoutPaymentFieldsPanel(String inTextLabel)
-	{
-		/*
-		 * creates a panel with a label and a field
-		 */
-		
-		super(); // call JPanel default constructor		
-		this.setLayout(new BorderLayout());
-		// TODO why isn't swing accepting these dimensions???
-		this.setPreferredSize(new Dimension(300, 40)); // 300 wide, 40 high - 40 possible too much!!
-		
-		// Add the label
-		text = new JLabel(inTextLabel);
-		text.setSize(100, 20);
-		this.add(text, BorderLayout.WEST); // FOCUSED LEFT
-		
-		// Add the associated field
-		field = new JTextField(20);
-		field.setSize(200, 20);
-		this.add(field, BorderLayout.EAST); // FOCUSED RIGHT
-	}
+
+
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4885426338224971236L;
+
+    private final JLabel text;
+    private final JTextField field;
+
+    public CheckoutPaymentFieldsPanel(String inTextLabel)
+    {
+        /*
+         * creates a panel with a label and a field
+         */
+
+        super(); // call JPanel default constructor
+        this.setLayout(new FlowLayout());
+
+        // Add the label
+        text = new JLabel(inTextLabel);
+        this.add(text); // FOCUSED LEFT
+
+        // Add the associated field
+        field = new JTextField();
+        field.setColumns(20);
+        this.add(field); // FOCUSED RIGHT
+    }
 }
--- newbuild/gui/CheckoutPaymentPanel.java
+++ newbuild/gui/CheckoutPaymentPanel.java
@@ -10,55 +10,59 @@
 package gui;
 
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
-public class CheckoutPaymentPanel extends JPanel 
+public class CheckoutPaymentPanel extends JPanel
 {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 635271050458474969L;
-	
-	private CheckoutPaymentFieldsPanel cardNumber;
-	private JTextField expiryMonth;
-	private JTextField expiryYear;
-	private JTextField securityCode;
-	private CheckoutPaymentFieldsPanel name;
-	private CheckoutPaymentFieldsPanel country;
-	private CheckoutPaymentFieldsPanel address;
-	private CheckoutPaymentFieldsPanel address2;
-	private CheckoutPaymentFieldsPanel state;
-	private CheckoutPaymentFieldsPanel zip;
-	private CheckoutPaymentFieldsPanel phone;
-	
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 635271050458474969L;
 
-	public CheckoutPaymentPanel()
-	{
-		
-        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		
-		
-		
-		// Card number row
+    private final CheckoutPaymentFieldsPanel cardNumber;
+    private final JTextField expiryMonth;
+    private final JTextField expiryYear;
+    private final JTextField securityCode;
+    private final CheckoutPaymentFieldsPanel name;
+    private final CheckoutPaymentFieldsPanel country;
+    private final CheckoutPaymentFieldsPanel address;
+    private final CheckoutPaymentFieldsPanel address2;
+    private final CheckoutPaymentFieldsPanel state;
+    private final CheckoutPaymentFieldsPanel zip;
+    private final CheckoutPaymentFieldsPanel phone;
+
+
+    public CheckoutPaymentPanel()
+    {
+
+        this.setLayout(new GridLayout(0,1));
+
+        this.setSize(new Dimension(this.getWidth(), 800));
+
+        // Card number row
         cardNumber = new CheckoutPaymentFieldsPanel("Card number:");
         this.add(cardNumber);
-		
-		// Expiry row
+
+        // Expiry row
         expiryMonth = new JTextField();
-        expiryMonth.setSize(40, 20); // enough width for 2 characters
+        expiryMonth.setColumns(2);
         expiryMonth.setText("mm");
-        
+
         expiryYear = new JTextField();
-        expiryYear.setSize(80, 20); // enough width for 4 characters
+        expiryYear.setColumns(4);
         expiryYear.setText("yyyy");
-        
+
         securityCode = new JTextField();
-        securityCode.setSize(60, 20); // enough width for 3 characters
-        
+        securityCode.setColumns(3);
+
         JPanel expiryRow = new JPanel();
         expiryRow.setLayout(new FlowLayout());
         expiryRow.add(new JLabel("Expiration date:"));
@@ -67,33 +71,33 @@ public class CheckoutPaymentPanel extends JPanel
         expiryRow.add(new JLabel("CVC:"));
         expiryRow.add(securityCode);
         this.add(expiryRow);
-        
-        
+
+
         // Name row
         name = new CheckoutPaymentFieldsPanel("Name:");
         this.add(name);
-        
+
         // country row
         country = new CheckoutPaymentFieldsPanel("Country:");
         this.add(country);
-        
+
         // address rows
         address = new CheckoutPaymentFieldsPanel("Address:");
         this.add(address);
         address2 = new CheckoutPaymentFieldsPanel("");
         this.add(address2);
-        
+
         // state row
         state = new CheckoutPaymentFieldsPanel("State:");
         this.add(state);
-        
+
         // country row
         zip = new CheckoutPaymentFieldsPanel("Zip:");
         this.add(zip);
-        
+
         // country row
         phone = new CheckoutPaymentFieldsPanel("Phone:");
         this.add(phone);
-		
-	}	
+
+    }
 }
