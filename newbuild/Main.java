970fb1cd9e68df243528e8680b072221d7a64fce
baj231 <baj231@mail.usask>
2012-03-03 00:14:53 -0600

    Cleaned out old, commented out code from Main.java Code used to load main frame.  Now that login frame is in use, it is no longer needed.  (Login frame loads main frame)
    Signed-off-by: baj231 <baj231@mail.usask>

--- newbuild/Main.java
+++ newbuild/Main.java
@@ -4,24 +4,9 @@ public class Main {
 
     public static void main(String[] args){
 
-        //TODO: Brad
-
         /*
-         * Initialize the GUI manager
+         * Show login window.
          */
-        //PanelsManager.initialise();
-
-        /*
-         * Construct the GUI
-         */
-        //MainFrame mainFrame = new MainFrame();
-        //mainFrame.setVisible(true);
-
-        /*
-         * Open the GUI to the default tab
-         */
-        //PanelsManager.goToDefaultPanel();
-
         LoginFrame loginFrame = new LoginFrame();
         loginFrame.setVisible(true);
 
