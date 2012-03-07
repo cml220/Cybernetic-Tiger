ccd833780a3747117871030a5a7741d82e1d901d
Adam Demkey <asd270@mail.usask.ca>
2012-03-07 15:44:15 -0600

    changes to readercontroller

--- newbuild/controllers/ReaderController.java
+++ newbuild/controllers/ReaderController.java
@@ -1,7 +1,17 @@
 package controllers;
+import model.Book;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import model.Book;
+import org.icepdf.ri.common.ComponentKeyBinding;
+import org.icepdf.ri.common.SwingController;
+import org.icepdf.ri.common.SwingViewBuilder;
+import org.icepdf.ri.util.PropertiesManager;
 
 public class ReaderController {
 
@@ -10,8 +20,41 @@ public class ReaderController {
 		
 	}
 	
-	public JPanel openBook(Book book)
+	public JPanel openBook(Book book) throws MalformedURLException
 	{
-		return null;
+		//this creates the URL
+		URL url = new URL(book.pdfURL);
+		//the SwingController is an icepdf class needed
+		SwingController controller = new SwingController();
+		
+		//the properties manager is needed to remove the open,print, and save functions
+        PropertiesManager properties = new PropertiesManager(System.getProperties(), ResourceBundle.getBundle(PropertiesManager.DEFAULT_MESSAGE_BUNDLE));
+        properties.setBoolean(PropertiesManager.PROPERTY_SHOW_UTILITY_OPEN, false);
+        properties.setBoolean(PropertiesManager.PROPERTY_SHOW_UTILITY_PRINT, false);
+        properties.setBoolean(PropertiesManager.PROPERTY_SHOW_UTILITY_SAVE, false);
+
+        SwingViewBuilder factory = new SwingViewBuilder(controller, properties);
+
+        JPanel viewerComponentPanel = factory.buildViewerPanel();
+
+        // add copy keyboard command
+        ComponentKeyBinding.install(controller, viewerComponentPanel);
+
+        // add interactive mouse link annotation support via callback
+        controller.getDocumentViewController().setAnnotationCallback(
+                new org.icepdf.ri.common.MyAnnotationCallback(
+                        controller.getDocumentViewController()));
+
+        JFrame applicationFrame = new JFrame();
+        applicationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        applicationFrame.getContentPane().add(viewerComponentPanel);
+
+        // Now that the GUI is all in place, we can open the PDF
+        controller.openDocument(url);
+
+        // show the component
+        applicationFrame.pack();
+        applicationFrame.setVisible(true);
+        return viewerComponentPanel;
 	}
 }
