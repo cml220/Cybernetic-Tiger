537c4c5827aee85514f9845faa526a1f14507729
Colin <cml220@mail.usask.ca>
2012-03-29 14:42:11 -0600

    pushed an old version of GetterProcess, replaced with new one

--- newbuild/src/dbprocess/GetterProcess.java
+++ newbuild/src/dbprocess/GetterProcess.java
@@ -1,9 +1,6 @@
 package dbprocess;
 
 import java.sql.Connection;
-
-import exceptions.CartException;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,14 +8,16 @@ import java.util.ArrayList;
 
 import model.Book;
 import model.Cart;
+import model.PaymentInfo;
 import model.User;
+import exceptions.CartException;
 
 /** Getter Processes for Database.
  * @author Colin
  */
 public class GetterProcess {
     /** db connection. */
-    private Connection conn;
+    private final Connection conn;
     /** process instance. */
     private static GetterProcess instance;
 
@@ -51,6 +50,7 @@ public class GetterProcess {
      * @param username    the user whose assoc. data is to be found
      * @return    the data (as a string) if found, otherwise null
      * @throws SQLException sql method fail
+     * @throws CartException cart method fail
      */
     protected final User getUserInfo(
             final String username) throws SQLException {
@@ -65,9 +65,17 @@ public class GetterProcess {
                 + "\"" + username + "\"");
         if (rs.next()) {
             rs2.next();
+            PaymentInfo info = getPaymentInfo(username);
+            ArrayList<Book> rentals = getBooksBy(DatabaseProcess.USERNAME, username);
+            Cart cart;
+            try {
+                cart = getUserCart(username);
+            } catch (CartException ce) {
+                cart = null;
+            }
             User u;
             u = new User(rs.getString("UserName"),
-                    false, rs2.getString("email"));
+                    false, rs2.getString("email"), rentals, info, cart);
             if (rs.getString("isAdmin").equals("Y")) { u.isAdmin = true; }
             else { u.isAdmin = false; }
             return u;
@@ -123,7 +131,7 @@ public class GetterProcess {
                     c.add((db.getBookByIsbn(isbn)));
                 }
             }
-           }
+        }
         else {
             return null;
         }
@@ -156,6 +164,11 @@ public class GetterProcess {
                     + "tblBook.ISBN=tblBookRental.BookISBN AND"
                     + " tblBookRental.UserName=\"" + query + "\";");
         }
+        else if (option == DatabaseProcess.DESCRIPTION && !query.equals("")) {
+            rs = stmt.executeQuery("SELECT * FROM "
+                    + "tblBook WHERE Description LIKE \"%"
+                    + query + "%\" ORDER BY Title;");
+        }
         else if (option == DatabaseProcess.CATALOGUE && query.equals("")) {
             rs = stmt.executeQuery("SELECT * FROM tblBook ORDER BY Title;");
         }
@@ -166,7 +179,7 @@ public class GetterProcess {
         while (rs.next()) {
             bookList.add(new Book(rs.getString("Title"), rs.getString("Author"),
                     rs.getDouble("Price"), rs.getString("Url"),
-                    rs.getInt("ISBN"), rs.getString("picUrl"),
+                    rs.getLong("ISBN"), rs.getString("picUrl"),
                     rs.getString("Description")));
         }
         if (bookList.size() == 0) {
@@ -191,7 +204,7 @@ public class GetterProcess {
         if (rs.next()) {
             return new Book(rs.getString("Title"), rs.getString("Author"),
                     rs.getDouble("Price"), rs.getString("Url"),
-                    rs.getInt("ISBN"), rs.getString("picURL"),
+                    rs.getLong("ISBN"), rs.getString("picURL"),
                     rs.getString("Description"));
         } else {    //no book found
             return null;
@@ -235,14 +248,38 @@ public class GetterProcess {
         rs = stmt.executeQuery("SELECT * FROM "
                 + "tblBook WHERE (ISBN=" + isbn + ") LIMIT 1;");
         if (rs.first()) {
-            info = "<html>Title: " + rs.getString("title")
-                    + "<br>Author: " + rs.getString("author")
-                    + "<br>ISBN: " + rs.getString("isbn")
+            info = "<html>Title: " + rs.getString("Title")
+                    + "<br>Author: " + rs.getString("Author")
+                    + "<br>ISBN: " + rs.getString("Isbn")
                     + "<br>Description: <br>"
-                    + rs.getString("description");
+                    + rs.getString("Description");
         } else {
             return "No info found!";
         }
         return info;
+    }
+
+    /** Get the payment info for a user
+     * @param username  the user
+     * @return  the payment info if gotten; null otherwise
+     * @throws SQLException sql method fail
+     */
+    protected final PaymentInfo getPaymentInfo(
+            String username) throws SQLException {
+        Statement stmt = conn.createStatement();
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT * FROM "
+                + "tblPaymentInfo WHERE (UserName=\"" + username + "\");");
+        if(rs.next()) {
+            return new PaymentInfo(rs.getString("CardNumber"),
+                    rs.getString("UserName"),rs.getString("Country"),
+                    rs.getString("Address"),rs.getString("Address2"),
+                    rs.getString("ExpMonth"),rs.getString("ExpYear"),
+                    rs.getString("SecurityCode"),rs.getString("State"),
+                    rs.getString("ZIP"),rs.getString("Phone"));
+        }
+        else {
+            return null;
+        }
     }
 }
